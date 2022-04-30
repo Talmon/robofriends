3f2974292c1ac2180094da435a6df85f412a65a5
talmon <talmon.mwakesi@roamtech.com>
2022-04-30 20:59:01 +0300

    Updates

---
+++ precache-manifest.30a3359ffeabdd9e9946a8c42a9952a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebbb9d90126545b189e1",
+    "url": "/robofriends/static/css/main.b0f2ee1c.chunk.css"
+  },
+  {
+    "revision": "ebbb9d90126545b189e1",
+    "url": "/robofriends/static/js/main.ebbb9d90.chunk.js"
+  },
+  {
+    "revision": "f12bf432d1b1b25fb939",
+    "url": "/robofriends/static/js/1.f12bf432.chunk.js"
+  },
+  {
+    "revision": "e0ce5fb89d916af1be1c",
+    "url": "/robofriends/static/css/2.0e4ff9e6.chunk.css"
+  },
+  {
+    "revision": "e0ce5fb89d916af1be1c",
+    "url": "/robofriends/static/js/2.e0ce5fb8.chunk.js"
+  },
+  {
+    "revision": "9e3480fd504eb470244e",
+    "url": "/robofriends/static/js/runtime~main.9e3480fd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "24a2bb94238dc891e320422ef1b504ae",
+    "url": "/robofriends/index.html"
+  }
+];
